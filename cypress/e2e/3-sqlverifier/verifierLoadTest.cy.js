e77f20fef6b621a5014016158a1f1eebb3c7fef0
Anastasiia Morozova <nmorozova283@gmail.com>
2023-12-06 22:05:55 +0200

    hw 5.1.2 created test cy project

---
+++ cypress/e2e/3-sqlverifier/verifierLoadTest.cy.js
@@ -0,0 +1,6 @@
+describe('Verifier Page Load Test', () => {
+    it('successfully loads Verifier page', () => {
+      cy.visit('https://sqlverifier-live-6e21ca0ed768.herokuapp.com/login?page=1&sort=id,ascn') 
+      cy.get('.col-md-12').should('exist');
+    });
+  });
